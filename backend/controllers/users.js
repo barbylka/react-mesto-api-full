050f451b539196d4b14973a7aa43b8befa5ec4d7
Irina Stafeeva <irina.staffeewa@gmail.com>
2022-05-31 19:11:14 +0200

    set samesite as none

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -140,6 +140,7 @@ const login = async (req, res, next) => {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
             sameSite: 'none',
+            secure: true,
           })
           .status(200).send({ token });
       }
